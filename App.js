95803cb59298d6caefe5cda6669e0f0aa0c0f65b
Ishaan Aggarwal <i_aggarwal1@u.pacific.edu>
2024-03-11 11:12:58 -0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapsComponent from './components/MapComponent'; // Make sure the path matches the location of your MapsComponent file
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <MapsComponent />
+      </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
